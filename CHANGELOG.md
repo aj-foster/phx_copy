9fe65574334fcab836fa2567d4f7492b3f6df838
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-03-22 21:07:48 -0400

    Prepare for publication

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## v0.1.0 (2022-03-22)
+
+* Initial release
+* **Add**: `mix phx.copy` task
+* **Add**: `Phoenix.Copy.watch/1` with Phoenix Endpoint watcher compatibility
--- mix.exs
+++ mix.exs
@@ -4,17 +4,22 @@ defmodule Phoenix.Copy.MixProject do
   def project do
     [
       app: :phoenix_copy,
-      version: "0.1.0",
+      version: "0.1.0-rc.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
+      # Package
+      description: "Copy static assets for your Phoenix app during development and deployment.",
+      package: package(),
+
       # Docs
       name: "Phoenix.Copy",
+      homepage_url: "https://github.com/aj-foster/phx_copy",
       source_url: "https://github.com/aj-foster/phx_copy",
       docs: [
         main: "readme",
-        extras: ["README.md", "LICENSE"]
+        extras: ["CHANGELOG.md", "README.md", "LICENSE"]
       ]
     ]
   end
@@ -27,8 +32,16 @@ defmodule Phoenix.Copy.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:file_system, "~> 0.2"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs CHANGELOG.md LICENSE mix.exs README.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/aj-foster/phx_copy"}
     ]
   end
 end
