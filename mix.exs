129e94f92fad13957b3af39de36f408d1fabede4
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-03-22 21:28:23 -0400

    Prepare rc.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Phoenix.Copy.MixProject do
   def project do
     [
       app: :phoenix_copy,
-      version: "0.1.0-rc.0",
+      version: "0.1.0-rc.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
