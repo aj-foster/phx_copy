c06fd6def788fa859dd859126f34611947e09d86
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-03-22 21:13:13 -0400

    Relax Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Phoenix.Copy.MixProject do
     [
       app: :phoenix_copy,
       version: "0.1.0-rc.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
