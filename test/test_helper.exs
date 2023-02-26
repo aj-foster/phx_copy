bb96cac8d8c75e8d3f5fb3f755ebc2b2979fbfb6
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-02-25 22:22:11 -0500

    Update CI to run multiple versions of Elixir+OTP

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,7 @@
+if System.get_env("CI") do
+  ExUnit.configure(formatters: [ExUnit.CLIFormatter, JUnitFormatter])
+end
+
 ExUnit.after_suite(fn _results ->
   Path.expand("../tmp", __DIR__)
   |> File.rm_rf!()
